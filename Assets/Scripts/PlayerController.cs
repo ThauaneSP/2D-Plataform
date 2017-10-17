9822b0a4a6d36d54b7b83b6adf5bdca453f8f88d
ThauaneSP <thataspg@gmail.com>
2017-10-17 10:58:16 -0200

    Last Lesson

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,18 +13,23 @@ public class PlayerController : MonoBehaviour {
 
 	Rigidbody2D rb;
 
+	Animator anim;
+
+	bool isJumping = false;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		sr = GetComponent<SpriteRenderer>();
+		anim = GetComponent<Animator>();
 	}
-	
+	 
 	// Update is called once per frame
 	void Update () {
 		
 		float horizontalInput = Input.GetAxisRaw("Horizontal"); // -1? esquerda , 1: direita
 		float HorizontalPlayerSpeed = HorizontalSpeed * horizontalInput;
-		if (HorizontalSpeed != 0) {
+		if (HorizontalPlayerSpeed != 0) {
 			MoveHorizontal(HorizontalPlayerSpeed);
 		}
 		else {
@@ -34,7 +39,11 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetButtonDown("Jump")) {
 			Jump();
 		}
+
+		ShowFalling();
+
 	}
+
 
 
 	void MoveHorizontal(float speed) {
@@ -46,13 +55,34 @@ public class PlayerController : MonoBehaviour {
 		else if (speed > 0f) {
 			sr.flipX = false;
 		}
+
+		if (!isJumping) {
+			anim.SetInteger("State", 2);
+		}
 	}
 
 	void StopMovingHorizontal () {
 		rb.velocity = new Vector2(0f , rb.velocity.y);
+		if (!isJumping) {
+			anim.SetInteger("State", 0);
+		}
+	}
+
+	void ShowFalling() {
+		if (rb.velocity.y < 0) {
+			anim.SetInteger("State", 3);
+		}
 	}
 
 	void Jump () {
+		isJumping = true;
 		rb.AddForce(new Vector2(0f , jumpspeed));
+		anim.SetInteger("State", 1);
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.layer == LayerMask.NameToLayer("Ground")) {
+			isJumping = false;
+		}
 	}
 }
