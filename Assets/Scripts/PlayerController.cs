309a83406acc2440c05dd7198eb8995b2b592f47
Thauane <thataspg@gmail.com>
2017-10-31 10:30:03 -0200

    Lil'Knight Double Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,6 +25,9 @@ public class PlayerController : MonoBehaviour {
 	public bool isGrounded;
 	public LayerMask whatIsGround;
 
+	bool canDoubleJump = false;
+	public float delayFouDoubleJump = 0.2f;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
@@ -94,8 +97,22 @@ public class PlayerController : MonoBehaviour {
 		isJumping = true;
 		rb.AddForce(new Vector2(0f , jumpspeed));
 		anim.SetInteger("State", 1);
+
+		Invoke("EnableDoubleJump", delayFouDoubleJump);
+
+		}
+
+		if (canDoubleJump && !isGrounded) {
+			rb.velocity = Vector2.zero;
+			rb.AddForce(new Vector2(0f , jumpspeed));
+			anim.SetInteger("State", 1);
+			canDoubleJump = false;	
 		}
 	}
+
+	void EnableDoubleJump() {
+		canDoubleJump = true;
+	} 
 
 	void OnCollisionEnter2D(Collision2D other) {
 		if (other.gameObject.layer == LayerMask.NameToLayer("Ground")) {
